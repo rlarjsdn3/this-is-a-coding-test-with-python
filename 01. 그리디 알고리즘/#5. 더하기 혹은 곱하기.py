1dad50b0cdaf2cac8571bd449ac01d09bce4243c
Kim KunWoo <rlarjsdn3@naver.com>
2022-08-20 16:35:05 +0900

    Create #5. 더하기 혹은 곱하기.py

---
+++ 01. 그리디 알고리즘/#5. 더하기 혹은 곱하기.py
@@ -0,0 +1,13 @@
+# 05. 곱하기 혹은 더하기 - p. 312
+
+data = input()
+result = int(data[0])
+
+for i in range(1, len(data)):
+    num = int(data[i])
+    if num<=1 or result<=1:
+        result += num
+    else:
+        result *= num
+
+print(result)        
