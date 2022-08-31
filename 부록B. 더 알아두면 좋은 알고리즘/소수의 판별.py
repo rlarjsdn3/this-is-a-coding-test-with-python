09da5e97d1d12a29c32ee412d01eae58c0f91ec9
Kim KunWoo <rlarjsdn3@naver.com>
2022-08-31 21:21:21 +0900

    Create 소수의 판별.py

---
+++ 부록B. 더 알아두면 좋은 알고리즘/소수의 판별.py
@@ -0,0 +1,12 @@
+# 소수의 판별
+
+def is_prime_number(x):
+    # 2부터 (x-1)까지의 모든 수를 확인하며
+    for i in range(2, x):
+        # x가 i로 나누어 떨어진다면
+        if x%i == 0:
+            return False # 소수가 아님
+    return True # 소수임
+
+print(is_prime_number(4))
+print(is_prime_number(7))
