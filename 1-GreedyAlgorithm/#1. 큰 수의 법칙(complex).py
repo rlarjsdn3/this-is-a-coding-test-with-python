80f8bb93d3c1598218eda6aabe0d0ffe9cdc2345
Zizi_Kim <rlarjsdn3@naver.com>
2022-12-26 15:46:15 +0900

    그리디 알고리즘 파일 이름 변경(테스트)

---
+++ 1-GreedyAlgorithm/#1. 큰 수의 법칙(complex).py
@@ -0,0 +1,13 @@
+# 01. 큰 수의 법칙(complex) - p. 92
+
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+result = (first*k+second) * (m//(k+1))
+result += first * (m%(k+1))
+    
+print(result)
---
+++ 1-GreedyAlgorithm/#1. 큰 수의 법칙(simple).py
@@ -0,0 +1,17 @@
+# 01. 큰 수의 법칙(simple) - p. 92
+
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+sum = 0
+for i in range(1, m+1):
+    if i%k==0:
+        sum += second
+        continue
+    sum += first
+    
+print(sum)
